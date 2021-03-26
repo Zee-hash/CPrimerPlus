4fdeae319dcf8d4a469e77ddb6158918954ef320
Zee-hash <62379579+Zee-hash@users.noreply.github.com>
2021-03-26 13:46:34 +0800

    add ignore file

---
+++ Chapter06/6.15.do_while.c
@@ -0,0 +1,16 @@
+/* do_while.c -- 退出循环条件 */
+#include <stdio.h>
+int main(void)
+{
+    const int secret_code = 13;
+    int code_entered;
+
+    do
+    {
+        printf("To enter the triskaidekaphobia therapy club, \n");
+        printf("pleast enter the secret code number: ");
+        scanf("%d", &code_entered);
+    } while (code_entered != secret_code);
+    printf("Congratulations! You are cured!\n");
+    return 0;   
+}
